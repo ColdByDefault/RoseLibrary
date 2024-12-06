cfa8fe1924ae94db15a2971fe149c7fe1215ad09
Yazan <abo.ayash.yazan@gmail.com>
2024-12-06 17:20:30 +0100

    debugg

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: "/RoseLibrary/", 
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0", 
+    port: 5173, 
+    open: true, 
   },
-  plugins: [],
-};
+  build: {
+    outDir: "dist", 
+    sourcemap: true, 
+  },
+});
