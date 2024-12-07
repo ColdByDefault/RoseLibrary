b48e05d2d7e4b3f7338dba91782e8697f80d7a0b
Yazan <abo.ayash.yazan@gmail.com>
2024-12-07 15:15:37 +0100

    debugged

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div>
       <h1 className="text-4xl text-red-400">FireBase</h1>
+      <h1 className="text-4xl text-red-400">You will NOT see when you register yet... but be sure it was saved!!</h1>
       <Auth />
     </div>
   );
