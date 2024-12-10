e0392fc2831ff7fed39f18e6f70c85e740ca479b
Yazan <abo.ayash.yazan@gmail.com>
2024-12-10 20:03:05 +0100

    debugged, fixed Background.jsx,

---
+++ src/components/docs/Docs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import RandomDotsBackground from "../main/Background";
+
+
+function Docs() {
+    return (
+        <>
+            <div className="relative w-full h-screen">
+                    <div className="">
+                        <RandomDotsBackground />
+                    </div>
+                <div className="relative z-10 text-center text-red-200">
+                <h1 className="text-4xl font-bold">Welcome to My Website</h1>
+                <p className="mt-2 text-lg">Enjoy the starry background!</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Docs;
--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+import RandomDotsBackground from "./Background";
+import "../../styles/title.css"
 
+
+function Home() {
+    return (
+      <div className="relative w-full h-screen">
+            <div className="">
+                <RandomDotsBackground />
+            </div>
+            <div class="content">
+              <div class="title text-focus-in">Welcome to <span class="text-focus-in">BeRich</span>  Library</div>
+              <p>Developer: ColdByDefault</p>
+            </div>
+      </div>
+    );
+  }
+  
+
+export default Home;
